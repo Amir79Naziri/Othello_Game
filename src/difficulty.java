3b0d13d6adaaef955b356ade81a25d463cebe68d
9726081 <9726081@ceit.local>
2020-03-30 19:31:11 +0430

    "Change in difficulty class"

--- src/difficulty.java
+++ src/difficulty.java
@@ -1,3 +1,4 @@
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 
@@ -18,52 +19,50 @@ public abstract class difficulty extends Game
 
     public Coordinate chooseEndForMachine ()
     {
-        HashMap<Coordinate,Integer> paths = new HashMap<> ();
-        for (HashMap<Coordinate,HashSet<Coordinate>> sug : getGameHandling ().suggestions ())
+        HashMap<Coordinate, Integer> mapOfEndToLength = new HashMap<> ();
+        for (HashMap<Coordinate, HashSet<Coordinate>> sug : getGameHandling ().suggestions ())
             for (Coordinate key : sug.keySet ())
-                for (Coordinate end : sug.get (key))
-                    paths.put (end,getGameHandling ().findBegins (end).size ());
-
-    if (difficulty.equals ("Easy"))
-        return findLessBegins (paths);
-    else
-        return findBiggerBegins (paths);
+                for (Coordinate end : sug.get (key)) {
+                    mapOfEndToLength.put (end, findLength (end, getGameHandling ().findBegins (end)));
+                }
+        if (difficulty.equals ("Easy"))
+            return findLessLength (mapOfEndToLength);
+        else
+            return findBiggerLength (mapOfEndToLength);
     }
 
-    private  Coordinate findLessBegins (HashMap<Coordinate,Integer> paths)
+    private  Coordinate findLessLength (HashMap<Coordinate,Integer> mapOfEndToNumOfBegins)
     {
-        int min = 0;
-        int counter = 0;
-        for (Coordinate end : paths.keySet ()) {
-            if (counter == 0)
-                min = paths.get (end);
-            else if (paths.get (end) < min)
-                    min = paths.get (end);
-            counter++;
-        }
-
-        for (Coordinate end : paths.keySet ())
-            if (paths.get (end).equals (min))
+        int min = Collections.min (mapOfEndToNumOfBegins.values ());
+        for (Coordinate end : mapOfEndToNumOfBegins.keySet ())
+            if (mapOfEndToNumOfBegins.get (end).equals (min))
                 return end;
         return null;
     }
 
-    private Coordinate findBiggerBegins (HashMap<Coordinate,Integer> paths)
+    private Coordinate findBiggerLength (HashMap<Coordinate,Integer> mapOfEndToNumOfBegins)
     {
-        int max = 0;
-        int counter = 0;
-        for (Coordinate end : paths.keySet ()) {
-            if (counter == 0)
-                max = paths.get (end);
-            else if (paths.get (end) > max)
-                max = paths.get (end);
-            counter++;
-        }
-
-        for (Coordinate end : paths.keySet ())
-            if (paths.get (end).equals (max))
+        int max = Collections.max (mapOfEndToNumOfBegins.values ());
+        for (Coordinate end : mapOfEndToNumOfBegins.keySet ())
+            if (mapOfEndToNumOfBegins.get (end).equals (max))
                 return end;
         return null;
+    }
+
+    private int findLength (Coordinate end, HashSet<Coordinate> begins)
+    {
+        int sum = 0;
+        for (Coordinate begin : begins)
+        {
+            int x = Math.abs (end.getX () - begin.getX ());
+            int y = Math.abs (end.getY () - begin.getY ());
+
+            if (x == 0)
+                sum += y;
+            else
+                sum += x;
+        }
+        return sum;
     }
 
 }
